bb24566e8c2546c3c0265c6686034c016d016fc9
Allain Louis <e1602246@ens-ensibsva-0021.univ-ubs.fr>
2020-02-06 13:21:43 +0100

    Continue TP Candy

--- INF2244_Prog_Web/TP3_Candy_Crush/candy.js
+++ INF2244_Prog_Web/TP3_Candy_Crush/candy.js
@@ -1,6 +1,8 @@
+
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
-  }
+}
 
 // On part sur le principe que le modele va générer une grille aléatoire puis tant
 // qu'il y a des explosions on continue à la remplir aléatoirement.
@@ -11,23 +13,25 @@ class Modele {
     constructor(taille = 10) {
 
         this.taille = taille
-        this.donnees = Array(this.taille).fill(Array(this.taille).fill(0))
-        this.NB_ALIGNE = 3
-
+        this.NB_ALIGNE = 3 // le nbre mini de bonbons à aligner pour les éclater
+        this.donnees = []
         this.initialiseGrille()
     }
 
     /**
-     * Rempli la grille aléatoirement tant qu'il peut y avoir
+     * Rempli la grille aléatoirement tant qu'ilgetRandomInt peut y avoir
      * des explosions.
      */
     initialiseGrille() {
         
-        for(let x in this.donnees) {
-            for(let y in this.donnees[x]) {
-                this.donnees[x][y] = getRandomInt(4)
+        let tmpBoard = []
+        for(let i = 0 ; i < this.taille ; i++){
+            tmpBoard[i] = Array()
+            for(let j = 0 ; j < this.taille ; j++){
+                tmpBoard[i][j] = getRandomInt(4) + 1
             }
         }
+        this.donnees = tmpBoard
     }
 
     /**
@@ -35,10 +39,10 @@ class Modele {
      * s'il n'y en a pas.
      */
     explosionPossible() {
-        aExploser = []
-        //for()
+        aExploser = [] // liste de coordonnées à exploser
+        
     }
 }
 
 var modele = new Modele()
-console.log(modele.donnees)
+console.table(modele.donnees)
