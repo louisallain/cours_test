2c1a69b22119f64c472d5bb18da572f263040604
e1602246 <allain.e1602246@etud.univ-ubs.fr>
2020-11-25 20:12:05 +0100

    Updates

---
+++ precache-manifest.9b2e004b3b5a5bf024f7d39a657354e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acd061e98f940ae20afeb53e922b0a91",
+    "url": "/cyberkey_admin/index.html"
+  },
+  {
+    "revision": "137a4079f7360d80bbc6",
+    "url": "/cyberkey_admin/static/css/main.c6fdb717.chunk.css"
+  },
+  {
+    "revision": "503bbcb9a02f0d9d2a1f",
+    "url": "/cyberkey_admin/static/js/2.5ec8bcb5.chunk.js"
+  },
+  {
+    "revision": "d822a0a3acb6aad7b3cb7e36794bc3b4",
+    "url": "/cyberkey_admin/static/js/2.5ec8bcb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "137a4079f7360d80bbc6",
+    "url": "/cyberkey_admin/static/js/main.54b6df05.chunk.js"
+  },
+  {
+    "revision": "33b8ea64be890eb89a3b",
+    "url": "/cyberkey_admin/static/js/runtime-main.e13cf59b.js"
+  }
+]);
