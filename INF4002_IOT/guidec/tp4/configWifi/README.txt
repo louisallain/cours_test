55c60b13474f08acd9333be25822814478e95e49
e1602246 <allain.e1602246@etud.univ-ubs.fr>
2020-12-10 15:03:03 +0100

    end tp config wifi

---
+++ INF4002_IOT/guidec/tp4/configWifi/README.txt
@@ -0,0 +1,12 @@
+- Ce programme permet de configurer des paramètres de connexion wifi et de connexion à un serveur pour
+l'envoi de données de température.
+
+- Pour basculer en mode configuration il faut mettre la pâte 14 de la carte à l'état haut, pour re-basculer
+en mode envoi de données il faut mettre cette même pâte à l'état bas.
+
+- En mode configuration, on peut accéder à une interface depuis l'adresse 10.0.0.1. Dans cette interface,
+il est possible de configurer le SSID et le mot de passe d'un point d'accès wifi ainsi que l'adresse et le port
+du serveur où envoyer les données en mode envoi de données.
+
+- En mode envoi de données, le programme collecte des données de température (fictive pour l'exemple) et les envoi
+au serveur préciser dans les paramètres et modifiable en mode configuration.
--- INF4002_IOT/guidec/tp4/configWifi/configWifi.ino
+++ INF4002_IOT/guidec/tp4/configWifi/configWifi.ino
@@ -24,6 +24,9 @@ Settings settings;
 
 // ==========================================================================
 
+// ==========================================================================
+// ACCESS POINT
+// ==========================================================================
 #define AP_SSID "esp32_"
 #define AP_PASS "bonjour56"
 
@@ -32,6 +35,161 @@ Settings settings;
 ESP32WebServer web_server(WEB_SERVER_PORT);
 WiFiClient client;
 String mac;         // MAC address of the local device
+// ==========================================================================
+
+#define SWITCH_MODE_BUTTON 14
+#define LED_WiFi  2
+
+wl_status_t WiFi_status = WL_NO_SHIELD;
+
+unsigned long WiFi_last_attempt               = 99999;
+static unsigned long WiFi_attempt_interval    = 10000;
+
+unsigned long last_sampling                   = 99999;
+static unsigned long sampling_interval        = 5000;
+
+// ==========================================================================
+// WI-FI MANAGEMENT
+// ==========================================================================
+
+// ==========================================================================
+// Set up the Wi_Fi client
+//
+// - Enable the LED that will display the connectivity status
+// - Enable mode WIFI_STA and display the MAC addresses
+// - Set the client's timeout to 5 seconds
+void setup_WiFi_client() {
+
+  // utilisés pour convertir l'adresse mac en String arduino
+  byte macBytes[6];
+  char macCharArray[18];
+  
+  pinMode(LED_WiFi, OUTPUT); // led du statut de la connexion wifi
+  
+  WiFi.mode(WIFI_STA); // wifi en mode station
+  WiFi.macAddress(macBytes); // récupération de l'adresse MAC de la carte
+
+  // conversion du tableau d'octets en String
+  sprintf(macCharArray, "%2X:%2X:%2X:%2X:%2X:%2X", macBytes[0], macBytes[1], macBytes[2], macBytes[3], macBytes[4], macBytes[5]);
+  mac = String(macCharArray);
+  Serial.println("MAC="); Serial.print(mac); Serial.println();
+
+  // timeout du client fixé à 5 secondes
+  client.setTimeout(5);
+}
+
+// ==========================================================================
+// Return 'true' if 'ssid' has been detected during the last scan.
+// Return 'false' otherwise.
+// n : number of ssid find by scan
+bool checkSSID(String ssid, int n) {
+
+  for(int i=0; i<n; i++) if(ssid == WiFi.SSID(i)) return true;
+  return false;
+}
+
+// ========================================================================
+// Return the channel number used by network 'ssid', as detected during
+// the last scan.  Return 0 if 'ssid' was not detected.
+// n : number of ssid find by scan
+int getChannel(String ssid, int n) {
+
+  for(int i=0; i<n; i++) if(ssid == WiFi.SSID(i)) return WiFi.channel(i);
+  return 0;
+}
+
+// ==========================================================================
+// Check the current WiFi status (i.e., connected or not connected).
+// When a connection or disconnection is detected, display information in
+// the serial console, and switch the LED on or off accordingly.
+// When a disconnection is detected, stop the WiFiClient if it was connected.
+void check_WiFi_status() {
+
+  WiFi_status = WiFi.status();
+  if(WiFi_status == WL_CONNECTED) {
+    digitalWrite(LED_WiFi, HIGH);
+  }
+  else {
+    digitalWrite(LED_WiFi, HIGH);
+  }
+}
+
+// ==========================================================================
+// — on scanne les réseaux environnants;
+// — on vérifie avec checkSSID() que le SSID du réseau visé a été détecté (sinon on abandonne);
+// — on extrait avec getChannel() le n° de canal sur lequel se trouve l’AP ayant ce SSID;
+// — on tente une connexion avec cet AP, en spécifiant le SSID, le mot de passe, et le canal utilisé
+void start_WiFi_connection() {
+
+  Serial.print("Tentative de connexion ");
+  Serial.println(settings.ssid);
+  Serial.println("Scan en cours...");
+  int numSsid = WiFi.scanNetworks();
+  if (numSsid == -1) {
+    Serial.println("Connexion Wifi impossible");
+    return;
+  }
+
+  bool ssidFound = checkSSID(settings.ssid, numSsid);
+  // Vérifie qu'on trouve bien l'AP ayant ce SSID
+  if(ssidFound) {
+    Serial.print(settings.ssid);
+    Serial.println(" Trouve");
+    int numChannel = getChannel(settings.ssid, numSsid);
+    if(numChannel != 0) {
+      Serial.println("Tentative d'association...");
+      WiFi.begin(settings.ssid, settings.passwd);
+      check_WiFi_status();
+    }
+  }
+  else {
+    Serial.println("SSID non trouve");
+  }
+}
+
+// ==========================================================================
+// DATA ACQUISITION MANAGEMENT
+// ==========================================================================
+
+// ==========================================================================
+// Try to get new samples, and return these samples as a C string (or NULL
+// if no samples were obtained)
+char *get_samples() {
+
+  // Dummy implementation: should be replaced by real data sampling
+  static char data[256];
+
+  unsigned long now = millis();
+  float temp = 18.0 + (now % 100) / 10.0;
+  strcpy(data, "id=");
+  strcat(data, mac.c_str());
+  strcat(data, " temp=");
+  sprintf(data+strlen(data), "%.1f", temp);
+
+  return data;
+}
+
+// ==========================================================================
+// Send data to the remote server (via a TCP session)
+void send(char *data) {
+
+  Serial.print("Connecting to server: ");
+  if (client.connect(settings.addr, settings.port)) {
+
+    Serial.println("OK");
+    Serial.print("Sending data: ");
+    Serial.println(data);
+
+    client.println(data);
+
+    Serial.println("Closing connection");
+    client.stop();
+  }
+  else {
+    Serial.println("failed");
+  }
+  Serial.println();
+}
 
 // ==========================================================================
 // SETTINGS MANAGEMENT
@@ -220,12 +378,7 @@ void setup_AP() {
   Serial.println(tmpIP);
 }
 
-// ==========================================================================
-void setup() {
-
-  // Initialization of the serial console
-  Serial.begin(115200);
-
+void setup_configMode() {
   setup_EEPROM();
 
   setup_AP();
@@ -235,8 +388,70 @@ void setup() {
   Serial.println(); Serial.println();
 }
 
+void setup_modeData() {
+  setup_WiFi_client();
+}
+
 // ==========================================================================
+void setup() {
+
+  // Button permettant de "switch" entre la mode configuration et le mode envoi de données
+  pinMode(SWITCH_MODE_BUTTON, INPUT);
+  digitalWrite(SWITCH_MODE_BUTTON, LOW);
+
+  // Initialization of the serial console
+  Serial.begin(115200);
+}
+
+// ==========================================================================
+boolean modeConfigInitDone = false;
+boolean modeDataInitDone = false;
 void loop() {
 
-  handle_Web_client();
+  int buttonState = digitalRead(SWITCH_MODE_BUTTON);
+   
+  if(buttonState == HIGH) { // mode config
+    
+    modeDataInitDone = false; // réinitialise l'envoi de données
+    if(modeConfigInitDone == false) {
+       setup_configMode();
+       modeConfigInitDone = true;
+    }
+    handle_Web_client();
+  }
+  else { // mode envoi de données
+    
+    modeConfigInitDone = false;
+    if(modeDataInitDone == false) {
+      setup_modeData();
+      modeDataInitDone = true;
+    }
+
+    unsigned long now = millis();
+
+    // Check if the Wi-Fi status has changed
+    check_WiFi_status();
+  
+    // If the Wi-Fi is currently "not connected"
+    if (WiFi_status != WL_CONNECTED) {
+      // If the last connection attempt was long ago, let us make another attempt
+      if (now - WiFi_last_attempt > WiFi_attempt_interval) {
+        WiFi_last_attempt = now;
+        start_WiFi_connection();
+      }
+      // There is no need to go further until the Wi-Fi connection is established
+      return;
+    }
+  
+    // If the last data acquisition was long ago, let us start another one
+    if (now - last_sampling > sampling_interval) {
+      last_sampling = now;
+  
+      // Try to get new samples
+      char *data = get_samples();
+  
+      // If new samples have been obtained, send these samples away
+      if (data != NULL) send(data);
+    }
+  }
 }
